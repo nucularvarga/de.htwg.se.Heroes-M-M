e30eb9ea630c5f27a93079b3686c92352cc4dc30
Janko <varga.janko@yahoo.de>
2019-03-29 13:00:26 +0100

    test .gitignore 1.1

--- src/main/scala/de/htwg/se/heroes/Feld.scala
+++ src/main/scala/de/htwg/se/heroes/Feld.scala
@@ -1,6 +1,6 @@
 package de.htwg.se.heroes
 
-class Feld(val rows: Vector[Vector[Tile]]) {
+case class Feld(val rows: Vector[Vector[Tile]]) {
   def this(size:Int) = this(Vector.tabulate(size, size){(row, col) => new Tile(0)})
   def replaceCell(row:Int, col:Int, cell:Tile):Feld = copy(rows.updated(row, rows(row).updated(col, cell)))
   def printf:Unit = {
