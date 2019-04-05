893cd9b3803ff972bf4204e8634845f5a06af413
Janko <varga.janko@yahoo.de>
2019-04-05 08:51:14 +0200

    exdent Field

--- src/main/scala/de/htwg/se/heroes/main.scala
+++ src/main/scala/de/htwg/se/heroes/main.scala
@@ -2,7 +2,7 @@ package de.htwg.se.heroes
 
 object entry{
   def main(args: Array[String]): Unit = {
-    val test = new Feld(10)
+    val test = new Feld(20)
     Worldbuilder.init(10, test)
     Worldbuilder.line(3,2,1,0,test)
 
