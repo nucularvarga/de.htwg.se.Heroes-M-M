38e8dd74705928fdb866fe9886020998a2afbb3d
Janko <varga.janko@yahoo.de>
2019-04-10 16:22:02 +0200

    modify turn based logic

--- src/main/scala/de/htwg/se/heroes/model/Playground.scala
+++ src/main/scala/de/htwg/se/heroes/model/Playground.scala
@@ -2,44 +2,65 @@ package de.htwg.se.heroes.model
 
 class Playground {
   val spielerarray = List(0, 1, 2, 3)
-  def main(args: Array[String]): Unit = {
+  def gameStart: Unit = {
     //var field = new Field(5)
 
     var x = false
-    for (a <- spielerarray) {
-      println("Spieler " + a + " ist dran")
-      val b = scala.io.StdIn.readLine()
-      b match {
-        case "w" => x = move(0)
-        case "a" => x = move(1)
-        case "s" => x = move(2)
-        case "d" => x = move(3)
-        case  _  => println(b + "ist keine gültige Eingabe")
+    while (true) {
+      for (a <- spielerarray) {
+        println("Spieler " + a + " ist dran")
+        var input = scala.io.StdIn.readLine()
+        while(!goodmove(input)) {
+            println("Spieler " + a + " ist dran")
+            input = scala.io.StdIn.readLine()
+          }
+        }
       }
-      if(!x) {
-        println(b + "ist kein gültiger zug")
-      }
+    }
+
+  def move(cell: Cell): Boolean = {
+    setCell(spielerarray(1).getX(), spielerarray(1).getY(), "Leer")
+    setCell(cell, "Player")
+    true
+  }
+
+  def goodmove(dir: String): Boolean = {
+    dir match {
+      case "w" => checkmove(0)
+      case "a" => checkmove(1)
+      case "s" => checkmove(2)
+      case "d" => checkmove(3)
+      case _ => false
     }
   }
 
-  def move(dir: Int): Boolean ={
-
+  def checkmove(dir: Int): Boolean ={
+    var cell = Leer()
     dir match {
-      case 0 => val cell = getCell(spielerarray(1).getY() + 1, val x = spielerarray(1).getX())
-      case 1 => val cell = getCell(spielerarray(1).getX() - 1 val x = spielerarray(1).getY())
-      case 2 => val cell = getCell(spielerarray(1).getY() - 1 val x = spielerarray(1).getX())
-      case 3 => val cell = getCell(spielerarray(1).getX() + 1 val x = spielerarray(1).getY())
+      case 0 =>  cell = getCell(spielerarray(1).getY() + 1, spielerarray(1).getX())
+      case 1 => cell = getCell(spielerarray(1).getX() - 1, spielerarray(1).getY())
+      case 2 => cell = getCell(spielerarray(1).getY() - 1, spielerarray(1).getX())
+      case 3 => cell = getCell(spielerarray(1).getX() + 1, spielerarray(1).getY())
       case _ => println("error")
     }
 
     cell match {
-      case Stop => false
-      case  _   => true
+      case Stop() => false
+      case Leer()  => move(cell)
     }
   }
 
   def getCell(x: Int, y: Int): Cell ={
     Field(x)(y)
+  }
+
+  def setCell(x: Int, y: Int, name: String): Unit ={
+    Field(x)(y)
+  }
+
+  def setCell(cell: Cell, name: String): Unit =
+  {
+
   }
 
 }
