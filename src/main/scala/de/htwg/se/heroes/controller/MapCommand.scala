bb088d91262dcbf0111c3bc1a91fe5b58dc5711e
Janko <varga.janko@yahoo.de>
2019-05-23 11:16:57 +0200

    redo is done

--- src/main/scala/de/htwg/se/heroes/controller/MapCommand.scala
+++ src/main/scala/de/htwg/se/heroes/controller/MapCommand.scala
@@ -10,17 +10,20 @@ class MapCommand(Map: MapMode, e: Event, controller: Controller) extends Command
 
   override def doStep: Unit =   controller.mode = controller.mode.handle(e)
 
-  override def undoStep: Unit = controller.mode = controller.mode.handle(inverseEvent(e))
-
+  override def undoStep: Unit = {
+    controller.playerBase.nextPlayer
+    controller.mode = controller.mode.handle(inverseEvent(e))
+    controller.playerBase.nextPlayer
+  }
   override def redoStep: Unit = controller.mode = Map
 
 
   def inverseEvent(d: Event): Event = {
     d match {
       case Event.MoveUp => Event.MoveDown
-      case Event.MoveLeft => Event.MoveLeft
+      case Event.MoveLeft => Event.MoveRight
       case Event.MoveDown => Event.MoveUp
-      case Event.MoveRight => Event.MoveRight
+      case Event.MoveRight => Event.MoveLeft
     }
   }
 
