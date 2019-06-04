71fcdafba853f6a2ce8063d9b3864e159f765339
Janko Varga <varga.janko@yahoo.de>
2019-06-04 21:38:04 +0200

    add image.

--- src/main/scala/de/htwg/se/heroes/aview/GUI.scala
+++ src/main/scala/de/htwg/se/heroes/aview/GUI.scala
@@ -6,31 +6,43 @@ import scalafx.scene.Scene
 import scalafx.scene.paint.Color._
 import scalafx.scene.shape.Rectangle
 import de.htwg.se.heroes.controller.Controller
+import de.htwg.se.heroes.main.controller
 import de.htwg.se.heroes.util.Observer
+import scalafx.application
 import scalafx.application.JFXApp.PrimaryStage
-import scalafx.stage.Stage
+import scalafx.scene.image.{Image, ImageView}
+import scalafx.stage.{Stage, WindowEvent}
 
 
 //class CellClicked(val row: Int, val column: Int) extends Event
 
-class GUI extends JFXApp {
 
-  def start(): Unit = {
-    // initialization here
-    stage = new PrimaryStage {
-      title.value = "HelloStage"
-      width = 600
-      height = 450
-      scene = new Scene {
-        fill = LightGreen
-        content = new Rectangle {
-          x = 25
-          y = 40
-          width = 100
-          height = 100
-          fill <== when(hover) choose Green otherwise Red
-        }
+
+object GUI extends JFXApp {
+
+  stage = new JFXApp.PrimaryStage {
+    title = "Hello Stage"
+    fullScreen_=(true)
+   // print(fullScreen.value)
+    /*scene = new Scene {
+      fill = LightGreen
+      content = new Rectangle {
+        x = 25
+        y = 40
+        width = 100
+        height = 100
+        fill <== when(hover) choose Green otherwise Red
       }
+    }*/
+    scene = drawTexture(new Image("file:src\\main\\scala\\de\\htwg\\se\\heroes\\aview\\teil1_3s.png"))
+  }
+
+
+  def drawTexture(typ: Image): Scene = {
+    new Scene(600,600) {
+      content = new ImageView(typ)
     }
   }
+
+
 }
--- src/main/scala/de/htwg/se/heroes/main.scala
+++ src/main/scala/de/htwg/se/heroes/main.scala
@@ -4,17 +4,18 @@ import de.htwg.se.heroes.aview.{GUI, Tui}
 import de.htwg.se.heroes.model._
 import de.htwg.se.heroes.controller.Controller
 
+
 import scala.io.StdIn.readLine
 
 object main{
   val controller = new Controller(new Field(9), new Arena(8, 20))
   val tui = new Tui(controller)
+  val gui = GUI
+
 
   //controller.notifyObservers
   def main(args: Array[String]): Unit = {
-    val gui = new GUI
     var input: String = ""
-    gui.start
     do {
       input = readLine()
       tui.processInputLine(input)
